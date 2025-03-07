36d6bf1be1afcb946f7e7d7951c45fcf62051f6f
razrahul <razrahuleee.com>
2025-03-08 03:15:42 +0530

    env setup

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
+const config = require("./database")[process.env.NODE_ENV || "development"];
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
   dialect: "mysql",
 //   logging: false, 
 });
