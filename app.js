6becf22fe67f25a495952a43fbaa2b4dc16cc830
razrahul <razrahuleee.com>
2025-03-08 01:43:34 +0530

    simple express server create

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 //root file (server ceated here)
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+//server listen
+const PORT = process.env.PORT || 3001;
+app.listen(3001, () => {
+  try {
+    console.log(`Server is running on port ${PORT}`);
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+});
