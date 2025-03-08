4b067462bf99b16772647a60b33a1e928d606244
razrahul <razrahuleee.com>
2025-03-08 20:03:56 +0530

    role& user model create

--- src/models/role.js
+++ src/models/role.js
@@ -1,1 +1,25 @@
-// role model
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/dbConnect");
+
+const Roles = sequelize.define("Roles", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  // Add other fields as needed
+},
+{
+    tableName: "roles",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+});
+
+
+module.exports = Roles;
